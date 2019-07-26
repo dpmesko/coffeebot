a50dc00b045ccc38fe49055b1221ea25cfdfcbfb
Daniel Mesko <dpmesko@gmail.com>
2019-07-26 18:28:23 -0400

    channelID not hardcoded - parsed from channel list requested from web api; multithreading and socket set up for receiving slash command payload from slack - next step, setup ngrok tunnel and parse slash command payload

--- coffeebot.py
+++ coffeebot.py
@@ -11,8 +11,11 @@
 
 import sys
 import slack 
-import json
 import datetime
+import time
+import threading
+import socket
+
 
 class coffeeOrder:
 
@@ -30,7 +33,8 @@ class coffeeOrder:
 	def __init__(self, channel):
 		self.channel = channel
 		self.out_file = open("orders.txt", 'w')
-	
+		self.lock = threading.Lock()
+		
 		date = datetime.date.today()
 
 		header = ('COFFEE ORDER FOR ' + date.isoformat() + '\n')
@@ -43,11 +47,27 @@ class coffeeOrder:
 			'blocks' : self.ORDERFORM
 		}
 
-	def addorder(self, user, order):
+	def add(self, user, order):
 
-		ordstr = 'User ' + user + ' wants ' + order
+		ordstr = user + ' wants ' + order + '\n'
 		self.out_file.write(ordstr)
 
+
+
+def handle_client(clntsock):
+	global order
+
+	response = b''
+	while True:
+		bit = clntsock.recv(1024)
+		if not bit:
+			break
+		response += bit
+
+	order.lock.acquire()
+	order.add('john', str(response))
+	order.lock.release()
+	sys.exit()
 
 if __name__ == '__main__':
 	
@@ -56,20 +76,50 @@ if __name__ == '__main__':
 		err_str = ('ERROR: Please include the filepath to your OAuth access token '
 			+ 'as the sole comamand line argument.')
 		print(err_str)
-		sys.exit()
+		sys.exit(1)
 
+	# TODO: Replace with environent variable 
 	token_file = open(sys.argv[1], 'r+')
 	token = token_file.readline()
 	token = token.rstrip('\n')
 
 	client = slack.WebClient(token)
-
-	order = coffeeOrder("CL9GHRNSH")
 	
+	# finds the channelID of a channel named coffee, stores it
+	response = client.channels_list()
+	channels = response.get("channels")
+
+	channel_id = None	
+	for x in channels:
+		if x['name'] == 'coffee':
+			channel_id = x['id']
+			break
+
+	if channel_id == None:
+		print('ERROR: No channel named \"coffee\" found in this workspace')
+		sys.exit(1)	
+	
+
+	# create order and post message
+	global order
+	order = coffeeOrder(channel_id)
 	response = client.chat_postMessage(**(order.getordermsg()))
 
-	if 'not_authed' in response:
-		print('Error: OAuth access token is invalid')
+	# listen to port 2112 for 15 minutes for slash command payload
+	#   from slack
+
+	timeout = time.time() + 60*15
+	socket = socket.socket()
+	socket.bind(('',2112))
+	socket.listen(5)
+
+	# TODO: timeout won't be checked until one final connection is accepted...
+	# use select()...
+	while timeout > time.time():
+		clntsock, addr = socket.accept()
+		thread = threading.Thread(target=handle_client, args=(clntsock,))
+		thread.start()
+
 
 
 
